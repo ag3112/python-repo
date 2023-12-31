4a8f173f8ed42e79d3fc14987341647685e83901
Ankit Gupta <ag_3112@outlook.com>
2023-12-31 14:46:30 +0530

    optimsed the solution

--- removeNthNode.py
+++ removeNthNode.py
@@ -7,16 +7,26 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        cNode = head
-        pNone = None
-        while True:
-            if cNode.next is None:
-                pNode.next = cNode
-                break
-            else:
-                pNode = cNode
-                cNode = cNode.next
+        lNode = head
+        node = head
+        pNode = None
 
+        nCount = 0
+        while True:
+            nCount += 1
+            if nCount > n:
+                pNode = node
+                node = node.next
+            if not lNode.next:
+                break
+            lNode = lNode.next
+
+        if pNode is None: # mean we have remove the first node
+            if nCount == 1:
+                return ListNode('')
+            head = node.next
+        else:
+            pNode.next = node.next
         return head
 
 
@@ -28,21 +38,13 @@ if __name__ == '__main__':
     l.next.next = ListNode(3)
     l.next.next.next = ListNode(4)
     l.next.next.next.next = ListNode(5)
-    l.next.next.next.next.next = ListNode(5)
-
-    print(l.val)
-    print(l.next.val)
-    print(l.next.next.val)
-    print(l.next.next.next.val)
-    print(l.next.next.next.next.val)
-
 
     s = Solution()
-    l = s.removeNthFromEnd(l, 2)
-
+    l = s.removeNthFromEnd(l, 1)
+    print('---------------------')
     print(l.val)
     print(l.next.val)
     print(l.next.next.val)
     print(l.next.next.next.val)
-    print(l.next.next.next.next.val)
+    # print(l.next.next.next.next.val)
 
