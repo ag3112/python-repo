ca2635a6f326038a0fc3bf0070a63242d0ac3ab2
anks <ag_3112@outlook.com>
2024-12-04 22:55:24 +0530

    test

--- videos/iter_tools/adventOfCode4.py
+++ videos/iter_tools/adventOfCode4.py
@@ -1,9 +1,6 @@
 with open('/Users/anks/Documents/abc4.txt', 'r') as file:
     grid = [list(a.strip()) for a in file]
 
-word = 'XMAS'
-
-
 def get_char(grid, grid_idx, line_idx):
     out = ''
     if grid_idx >= 0 and line_idx >= 0:
@@ -14,7 +11,7 @@ def get_char(grid, grid_idx, line_idx):
     return out
 
 
-def get_combinations(grid):
+def get_xmas_combinations(grid):
     all_combinations = []
     for grid_idx in range(0, len(grid)):
         for line_idx in range(0, len(grid[grid_idx])):
@@ -26,9 +23,10 @@ def get_combinations(grid):
                                                                                                               grid_idx,
                                                                                                               line_idx - 2) + get_char(
                 grid, grid_idx, line_idx - 3)
-            forward_diagonal = get_char(grid, grid_idx, line_idx) + get_char(grid, grid_idx + 1, line_idx + 1) + get_char(grid,
-                                                                                                                  grid_idx + 2,
-                                                                                                                  line_idx + 2) + get_char(
+            forward_diagonal = get_char(grid, grid_idx, line_idx) + get_char(grid, grid_idx + 1,
+                                                                             line_idx + 1) + get_char(grid,
+                                                                                                      grid_idx + 2,
+                                                                                                      line_idx + 2) + get_char(
                 grid, grid_idx + 3, line_idx + 3)
             bottom = get_char(grid, grid_idx, line_idx) + get_char(grid, grid_idx + 1, line_idx) + get_char(grid,
                                                                                                             grid_idx + 2,
@@ -45,15 +43,15 @@ def get_combinations(grid):
                 grid, grid_idx - 3, line_idx - 3)
 
             backward_diagonal = get_char(grid, grid_idx, line_idx) + get_char(grid, grid_idx + 1,
-                                                                             line_idx - 1) + get_char(grid,
-                                                                                                      grid_idx + 2,
-                                                                                                      line_idx - 2) + get_char(
+                                                                              line_idx - 1) + get_char(grid,
+                                                                                                       grid_idx + 2,
+                                                                                                       line_idx - 2) + get_char(
                 grid, grid_idx + 3, line_idx - 3)
 
             backward_reverse_diagonal = get_char(grid, grid_idx, line_idx) + get_char(grid, grid_idx - 1,
-                                                                              line_idx + 1) + get_char(grid,
-                                                                                                       grid_idx - 2,
-                                                                                                       line_idx + 2) + get_char(
+                                                                                      line_idx + 1) + get_char(grid,
+                                                                                                               grid_idx - 2,
+                                                                                                               line_idx + 2) + get_char(
                 grid, grid_idx - 3, line_idx + 3)
 
             all_combinations.append(forward)
@@ -68,6 +66,42 @@ def get_combinations(grid):
     return all_combinations
 
 
+def get_x_mas_combinations(grid):
+    count = 0
+    for grid_idx in range(0, len(grid)):
+        for line_idx in range(0, len(grid[grid_idx])):
+
+            if grid[grid_idx][line_idx] == 'A':
+                # check the circumference - forward MS A MS
+                if get_char(grid, grid_idx - 1, line_idx - 1) == 'M' and get_char(grid, grid_idx - 1,
+                                                                                  line_idx + 1) == 'S' and get_char(
+                    grid, grid_idx + 1, line_idx - 1) == 'M' and get_char(grid, grid_idx + 1, line_idx + 1) == 'S':
+                    count += 1
+
+                # check the circumference - backward SS A MM
+                if get_char(grid, grid_idx - 1, line_idx - 1) == 'S' and get_char(grid, grid_idx - 1,
+                                                                                  line_idx + 1) == 'S' and get_char(
+                    grid, grid_idx + 1, line_idx - 1) == 'M' and get_char(grid, grid_idx + 1, line_idx + 1) == 'M':
+                    count += 1
+
+                # check the circumference - right SM A SM
+                if get_char(grid, grid_idx - 1, line_idx - 1) == 'S' and get_char(grid, grid_idx - 1,
+                                                                                  line_idx + 1) == 'M' and get_char(
+                    grid, grid_idx + 1, line_idx - 1) == 'S' and get_char(grid, grid_idx + 1, line_idx + 1) == 'M':
+                    count += 1
+
+                # check the circumference - left SM A SM
+                if get_char(grid, grid_idx - 1, line_idx - 1) == 'M' and get_char(grid, grid_idx - 1,
+                                                                                  line_idx + 1) == 'M' and get_char(
+                    grid, grid_idx + 1, line_idx - 1) == 'S' and get_char(grid, grid_idx + 1, line_idx + 1) == 'S':
+                    count += 1
+
+    return count
+
+
 # print(get_combinations(grid))
-all_combinations = get_combinations(grid).count(word)
-print(all_combinations)
+all_combinations = get_xmas_combinations(grid).count('XMAS')
+print('A', all_combinations)
+
+count = get_x_mas_combinations(grid)
+print('B', count)
